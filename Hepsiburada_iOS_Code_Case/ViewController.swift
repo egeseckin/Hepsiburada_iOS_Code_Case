7b023c8f578a1b2d3e625e26289ba01c7b72364c
egeseckin <ege.seckin@outlook.com>
2021-10-24 20:22:09 +0300

    Initial Commit

---
+++ Hepsiburada_iOS_Code_Case/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Hepsiburada_iOS_Code_Case
+//
+//  Created by Ege Seçkin on 24.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Hepsiburada_iOS_Code_CaseUITests/Hepsiburada_iOS_Code_CaseUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Hepsiburada_iOS_Code_CaseUITestsLaunchTests.swift
+//  Hepsiburada_iOS_Code_CaseUITests
+//
+//  Created by Ege Seçkin on 24.10.2021.
+//
+
+import XCTest
+
+class Hepsiburada_iOS_Code_CaseUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
