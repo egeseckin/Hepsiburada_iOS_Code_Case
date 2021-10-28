f49a600e70051812dce3a9846411b9ffaefb7fc8
egeseckin <ege.seckin@outlook.com>
2021-10-28 12:28:59 +0300

    Data taken in order, to cells

--- Hepsiburada_iOS_Code_Case/Controller/ResultViewController.swift
+++ Hepsiburada_iOS_Code_Case/Controller/ResultViewController.swift
@@ -10,20 +10,51 @@ import UIKit
 var iTunesURL = "https://itunes.apple.com/search?"
 
 
-
-
 class ResultViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
     
+    var dataresult = [iTunesApiData]()
         
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         
-        
         collectionView.delegate = self
         collectionView.dataSource = self
         //collectionView.collectionViewLayout = UICollectionViewFlowLayout()
+        //collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "iTunesCollectionViewCell")
+        
+        let url = URL(string: iTunesURL)
+           //Create URL Session
+            let session = URLSession(configuration: .default)
+           
+           //Give a task to the session
+           //Closure
+            let task = session.dataTask(with: url!) { data, response, error in
+                if error != nil {
+                    print(error!)
+                }
+                else{
+                        do{
+                            let datadecoded = try JSONDecoder().decode(iTunesApiData.self, from: data!)
+                            self.dataresult = [datadecoded]
+                            
+                            
+                        }catch{
+                            print(error)
+                        }
+                        
+                        DispatchQueue.main.asyncAfter(deadline: .now()) {
+                                
+                               
+                        }
+                       
+                }
+            }
+        //Start the task
+        task.resume()
+
+
     }
     
   
@@ -39,43 +70,12 @@ class ResultViewController: UIViewController, UICollectionViewDelegate, UICollec
     }
     
      func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "iTunesCollectionViewCell", for: indexPath) as! iTunesCollectionViewCell
-         if let url = URL(string: iTunesURL){
-            //Create URL Session
-            let session = URLSession(configuration: .default)
-            
-            //Give a task to the session
-            //Closure
-            let task =  session.dataTask(with: url) { data, response, error in
-                if error != nil {
-                    print(error!)
-                }
-                if let safeData = data{
-                    let decoder = JSONDecoder()
-                    do{
-                        
-                        let decodedData = try decoder.decode(iTunesApiData.self, from: safeData)
-                        sleep(1)
-                        if (decodedData.resultCount != 0){
-                            DispatchQueue.main.asyncAfter(deadline: .now()) {
-                                cell.setup(with: decodedData)
-                            
-                            }
-                        }
-                        else{
-                            print("No data")
-                        }
-                        
-                    }catch{
-                        print(error)
-                    }
-                }
-            }
-            //Start the task
-            task.resume()
+         
+        DispatchQueue.main.asyncAfter(deadline: .now()) {
+             cell.setup(with: self.dataresult)
         }
-        
+         
         return cell
     }
 }
--- Hepsiburada_iOS_Code_Case/Model/iTunesCollectionViewCell.swift
+++ Hepsiburada_iOS_Code_Case/Model/iTunesCollectionViewCell.swift
@@ -17,19 +17,19 @@ class iTunesCollectionViewCell: UICollectionViewCell {
     
     
     
-    func setup(with items: iTunesApiData ){
+    func setup(with items: [iTunesApiData] ){
 
-            let imageUrl = URL(string: items.results[count].artworkUrl100)!
-            let imageData = try! Data(contentsOf: imageUrl)
-            self.iTunesImage.image = UIImage(data: imageData)
+        let imageUrl = URL(string: items[0].results[count].artworkUrl100)!
+        let imageData = try! Data(contentsOf: imageUrl)
+        self.iTunesImage.image = UIImage(data: imageData)
+                    
+        self.nameLabel.text = "Name: " + items[0].results[count].collectionName
+        self.priceLabel.text = "Price: " + String(items[0].results[count].collectionPrice) + "$"
+                    
+        let date = items[0].results[count].releaseDate.components(separatedBy: "T")
+        self.dateLabel.text = "Date: " + date[0]
             
-            self.nameLabel.text = "Name: " + items.results[count].collectionName
-            self.priceLabel.text = "Price: " + String(items.results[count].collectionPrice) + "$"
-            
-            let date = items.results[count].releaseDate.components(separatedBy: "T")
-            self.dateLabel.text = "Date: " + date[0]
-            
-            count += 1
+        count += 1
         
     }
 }
